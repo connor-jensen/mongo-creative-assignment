03c42e32fb3140e84014461f409e3582407c677a
Connor Jensen <connor.jensen@live.com>
2018-11-16 04:16:28 +0000

    finished project

---
+++ server.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+var Message = mongoose.model('Message', {
+  name: String,
+  message: String
+})
+var dbUrl = 'mongodb://CS260:mongocreative260@ds037077.mlab.com:37077/mongo-crative-project';
+app.get('/messages', (req, res) => {
+  Message.find({}, (err, messages) => {
+    res.send(messages);
+  })
+})
+app.get('/messages', (req, res) => {
+  Message.find({}, (err, messages) => {
+    res.send(messages);
+  })
+})
+app.post('/messages', (req, res) => {
+  var message = new Message(req.body);
+  message.save((err) => {
+    if (err)
+      sendStatus(500);
+    io.emit('message', req.body);
+    res.sendStatus(200);
+  })
+})
+io.on('connection', () => {
+  console.log('a user is connected')
+})
+mongoose.connect(dbUrl, { useNewUrlParser: true}, (err) => {
+  console.log('mongodb connected', err);
+})
+var server = http.listen(8080, () => {
+  console.log('server is running on port', server.address().port);
+});
